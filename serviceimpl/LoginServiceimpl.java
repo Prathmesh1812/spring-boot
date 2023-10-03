172d441aa0db8cf86957292d85cfeed97cf5434d
Prathmesh1812 <139982211+Prathmesh1812@users.noreply.github.com>
2023-10-03 18:38:53 +0530

    Update LoginServiceimpl.java

--- serviceimpl/LoginServiceimpl.java
+++ serviceimpl/LoginServiceimpl.java
@@ -1,1 +1,22 @@
+package com.bank.sb.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bank.sb.entity.Login;
+import com.bank.sb.repository.LoginRepository;
+import com.bank.sb.service.LoginService;
+
+@Service
+public class LoginServiceImpl implements LoginService{
+	
+	@Autowired
+	LoginRepository lrepo;
+
+	@Override
+	public Login loginUser(String userName, String password) {
+		Login login = lrepo.findByUserNameAndPassword(userName, password);
+		return login;
+	}
+
+}
